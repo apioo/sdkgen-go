a4a5367d439350e60e4f4bf33b73c6f4cc1b5743
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-09-04 20:21:29 +0200

    add json info to struct

--- access_token.go
+++ access_token.go
@@ -1,9 +1,9 @@
 package sdkgen
 
 type AccessToken struct {
-	TokenType    string
-	AccessToken  string
-	ExpiresIn    int64
-	RefreshToken string
-	Scope        string
+	TokenType    string `json:"token_type"`
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int64  `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	Scope        string `json:"scope"`
 }
