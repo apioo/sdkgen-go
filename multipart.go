72c3f1e36e279e604a7bd352e5e81d0ecbad4cb6
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-09-23 21:16:34 +0200

    add multipart

---
+++ multipart.go
@@ -0,0 +1,50 @@
+package sdkgen
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+)
+
+type Multipart struct {
+	files  []FilePart
+	fields []FieldPart
+}
+
+func (multi *Multipart) AddFile(name string, fileName string, reader io.Reader) {
+	multi.files = append(multi.files, FilePart{name, fileName, reader})
+}
+
+func (multi *Multipart) AddField(name string, reader io.Reader) {
+	multi.fields = append(multi.fields, FieldPart{name, reader})
+}
+
+func (multi *Multipart) Build() *bytes.Buffer {
+	var reqBody = &bytes.Buffer{}
+	writer := multipart.NewWriter(reqBody)
+
+	for _, file := range multi.files {
+		part, _ := writer.CreateFormFile(file.Name, file.FileName)
+		io.Copy(part, file.Reader)
+	}
+
+	for _, field := range multi.fields {
+		part, _ := writer.CreateFormField(field.Name)
+		io.Copy(part, field.Reader)
+	}
+
+	writer.Close()
+
+	return reqBody
+}
+
+type FilePart struct {
+	Name     string
+	FileName string
+	Reader   io.Reader
+}
+
+type FieldPart struct {
+	Name   string
+	Reader io.Reader
+}
